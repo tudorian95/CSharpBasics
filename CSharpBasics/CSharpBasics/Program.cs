941ac2f7f6870001969d1255f4bd3a27bdcf6783
tudorian95 <tudorian95@gmail.com>
2023-03-28 12:48:18 +0300

    Part 1 homework
    basic integer operations

---
+++ CSharpBasics/CSharpBasics/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.WriteLine("Hello there");
+/* studied:
+ - breakpoints
+ - variables (int/double a.k.a float/char/bool/string)
+*/
+Console.WriteLine("You'll now be prompted to input a numeric value");
+int box = int.Parse(Console.ReadLine());
+Console.WriteLine("Your value is:{0}", box);
+
+//to do: GIT used to it + operations on ints
+Console.WriteLine("Some basic integer operations with your provided value: \n");
+Console.WriteLine("Addition\n What value should we add to your initial choice?");
+Console.WriteLine(box + int.Parse(Console.ReadLine()));
+Console.WriteLine("\nSubtraction\n What value should we subtract from your initial choice?");
+Console.WriteLine(box - int.Parse(Console.ReadLine()));
+Console.WriteLine("\nMultiplication\n By what value should we multiply your initial choice?");
+Console.WriteLine(box * int.Parse(Console.ReadLine()));
+Console.WriteLine("\nDivision\n By what value should we divide your initial choice?");
+Console.WriteLine(box / int.Parse(Console.ReadLine()));
+Console.WriteLine("\nModulo\n For which value should we check the integer remainder after dividing your initial choice?");
+Console.WriteLine(box % int.Parse(Console.ReadLine()));
