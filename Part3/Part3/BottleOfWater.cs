063a6feaf9d4fef1c3143773a4c5ccb1902a67ad
tudorian95 <tudorian95@gmail.com>
2023-03-29 14:34:14 +0300

    Part3 Homework done
    Implemented a small bottle class that has a few attributes and functions

---
+++ Part3/Part3/BottleOfWater.cs
@@ -0,0 +1,49 @@
+﻿public class BottleOfWater
+{
+    public string Vendor { get; set; }
+    public int Capacity { get; set; }
+
+    bool PickedUp { get; set; }
+
+    public BottleOfWater(string vendor, int capacity, bool pickedUp)
+    {
+        Vendor = vendor;
+        Capacity = capacity;
+        PickedUp = pickedUp;
+    }
+
+    public void PickUpBottle()
+    {
+        if (!PickedUp)
+        {
+            PickedUp = true;
+        }
+    }
+
+    public void PlaceBottleDown()
+    {
+        PickedUp = false;
+    }
+
+    public void TakeASip(int sizeOfSip)
+    {
+        if (Capacity > 0)
+        {
+            if (sizeOfSip <= Capacity)
+            {
+                Capacity -= sizeOfSip;
+                Console.WriteLine("that was nice, your character's thirst has been quenched");
+            }
+            else
+            {
+                Capacity = 0;
+                Console.WriteLine("not quite there, but still something");
+            }
+        }
+        else
+        {
+            Console.WriteLine("uh, oh, no more water left!");
+        }
+
+    }
+}
---
+++ Part3/Part3/Program.cs
@@ -0,0 +1,7 @@
+﻿BottleOfWater bottle = new BottleOfWater("Borsec", 500, false);
+
+Console.WriteLine(bottle.Vendor);
+Console.WriteLine(bottle.Capacity);
+bottle.TakeASip(600);
+Console.WriteLine(bottle.Capacity);
+bottle.TakeASip(600);
